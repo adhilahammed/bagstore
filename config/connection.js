fa858ac087e546d9812a833304f7e81005814959
adhilahammed <adhilahammed0@gmail.com>
2022-10-24 09:38:23 +0530

    first commit

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/Ecart",{
+mongoose.connect("mongodb+srv://adhil:72wC3dYCPhhNpvFY@bagstores.fgdzocv.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true
 }).then(()=>{
     console.log('connection Successfull')
 }).catch((e)=>{
-    console.log('No Connection');
+    console.log('No Connection');  
 })
+
+//72wC3dYCPhhNpvFY
